b29b9a11625755b2ace9d0666c22c13595119e2f
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-04-16 02:20:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from app.routers import mail, map, payment, checkPayment
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or your domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
